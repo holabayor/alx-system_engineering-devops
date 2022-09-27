a5bacdb0786b2818757facc92775f1825f3088ac
holabayor <liasu.olabayo@gmail.com>
2022-09-27 20:25:06 +0100

    puppet to customize header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,9 @@
+# Nginx configuration: header customization
+
+exec {'Customize header':
+  command  => 'sudo apt update;
+  sudo apt install -y nginx;
+  sudo sed -i '57 a \\tadd_header X-Served-By \$hostname;' /etc/nginx/sites-available/default  
+  sudo /etc/init.d/nginx restart,
+  provider => 'shell'
+}
