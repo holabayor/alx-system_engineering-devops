af647b3a9f4585929747e76c4d582e6af543fe8f
holabayor <liasu.olabayo@gmail.com>
2022-09-27 20:39:45 +0100

    puppet to customize header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,6 +4,6 @@ exec {'Customize header':
   command  => 'sudo apt update;
   sudo apt install -y nginx;
   sudo sed -i '53 a \\tadd_header X-Served-By \$hostname;' /etc/nginx/sites-available/default;
-  sudo /etc/init.d/nginx restart',
+  sudo service nginx restart',
   provider => 'shell'
 }
