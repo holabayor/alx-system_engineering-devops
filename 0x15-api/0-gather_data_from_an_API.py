cbf8307ca39844de3b2eaab3889a0b1d0a6b84c5
holabayor <liasu.olabayo@gmail.com>
2022-10-17 16:13:22 +0100

    PEP 8 style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-""" Script that uses REST API to return info about a todo list progress
+"""
+Script that uses REST API to return info about a todo list progress
 """
 import requests
 from sys import argv
 
 
 def gather_data(emp_id):
-    """
-    Function to runn the script
-    """
+    """ Function to run the script """
 
-    user = requests.get("https://jsonplaceholder.typicode.com/\
-                         users/{}".format(emp_id)).json()
-    todos = requests.get("https://jsonplaceholder.typicode.com/\
-                          todos?userId={}".format(emp_id)).json()
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(emp_id)).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                        .format(emp_id)).json()
     completed_tasks = []
-    for task in todos:
+    for task in todo:
         if task.get("completed"):
             completed_tasks.append(task)
     print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
                                                           len(completed_tasks),
-                                                          len(todos)))
+                                                          len(todo)))
     for task in completed_tasks:
         print("\t {}".format(task.get("title")))
 
