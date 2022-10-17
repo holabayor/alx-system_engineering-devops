8f6c52f725a353ff5815cb7d0a9ec09f944dbd7e
holabayor <liasu.olabayo@gmail.com>
2022-10-17 23:08:47 +0100

    export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,26 +2,20 @@
 """
 Script that uses REST API to return info about a todo list progress
 """
-import csv
-import requests
-from sys import argv
-
-
-def export_data(emp_id):
-    """ Function to run the script """
+if __name__ == "__main__":
+    export_data(argv[1])
+    import csv
+    import requests
+    from sys import argv
 
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(emp_id)).json()
     todo = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
                         .format(emp_id)).json()
 
-    filename = "{}.csv".format(emp_id)
+    filename = emp_id + ".csv"
     with open(filename, 'w') as csvfile:
         csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todo:
             csvwriter.writerow([user.get("id"), user.get("username"),
                                task.get("completed"), task.get("title")])
-
-
-if __name__ == "__main__":
-    export_data(argv[1])
