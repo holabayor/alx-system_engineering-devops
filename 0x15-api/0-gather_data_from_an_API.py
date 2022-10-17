5a8ff4c5f9f6228093f1f52653dac1fefe73afbb
holabayor <liasu.olabayo@gmail.com>
2022-10-17 15:55:14 +0100

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# Script that uses REST API to return info about a todo list progress
+import requests
+from sys import argv
+
+
+def gather_data(emp_id):
+    """
+    Function to runn the script
+    """
+
+    user = requests.get("https://jsonplaceholder.typicode.com/\
+                         users/{}".format(emp_id)).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/\
+                          todos?userId={}".format(emp_id)).json()
+    completed_tasks = []
+    for task in todos:
+        if task.get("completed"):
+            completed_tasks.append(task)
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+                                                          len(completed_tasks),
+                                                          len(todos)))
+    for task in completed_tasks:
+        print("\t {}".format(task.get("title")))
+
+
+if __name__ == "__main__":
+    gather_data(argv[1])
