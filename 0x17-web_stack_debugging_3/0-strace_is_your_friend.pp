d2827d6b754bb263a621d52f831ada5a6ac86ef5
holabayor <liasu.olabayo@gmail.com>
2022-11-01 13:18:46 +0100

    Debugging with Puppet

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Find apache error and fix with puppet
+
+exec { 'correct filename':
+  provider => shell,
+  command  => 'sed -i s/phpp/php /var/www/html/wp-settings.php',
+}
+
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,2 @@
+0x17 - Web Stack Debugging
+This task uses puppet to fix error in wordpress config file
