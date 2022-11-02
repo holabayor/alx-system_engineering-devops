1618099b4a5497039330431d7d8f1cdece9d620c
holabayor <liasu.olabayo@gmail.com>
2022-11-02 13:50:54 +0100

    Debugging with Puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 
 exec { 'correct filename':
   provider => shell,
-  command  => 'sed -i s/phpp/php /var/www/html/wp-settings.php',
+  command  => "sed -i 's/.phpp/.php' /var/www/html/wp-settings.php",
 }
 
